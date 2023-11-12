24b226ef4dfcf16c5cdcf5ffcb1d2da941aa759b
Makarand Milind Hinge <109909013+makarandhinge@users.noreply.github.com>
2023-11-12 20:26:35 +0530

    Todo-list version 1.0

---
+++ src/components/TodoItem1.jsx
@@ -0,0 +1,17 @@
+function TodoItem1() {
+  return (
+    <div class="container">
+      <div class="row mk-row">
+        <div class="col-6">Buy Milk</div>
+        <div class="col-4">4/10/2023</div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger mk-button">
+            DELETE
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem1;
---
+++ src/components/TodoItem2.jsx
@@ -0,0 +1,17 @@
+function TodoItem2() {
+  return (
+    <div class="container">
+      <div class="row mk-row">
+        <div class="col-6">Go To College</div>
+        <div class="col-4">4/10/2023</div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger mk-button">
+            DELETE
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem2;
